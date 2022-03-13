cb38ee8cf31f8935f45a4901b737aeea42539b72
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-03-13 18:20:04 -0400

    Redirected to user home if already logged in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit {
     private authService: AuthService
   ) {
     this.createForm();
+    this.authService.navigateUserHome();
   }
 
   ngOnInit(): void {
