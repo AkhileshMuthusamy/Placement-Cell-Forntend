f1372de5bf06b642b9fcff3f5e73ada1bd17b34a
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-03-13 20:19:04 -0400

    Updated layout for Faculty, Student and Placement

--- src/app/faculty/faculty-layout.component.ts
+++ src/app/faculty/faculty-layout.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MediaObserver} from '@angular/flex-layout';
+import {MatDialog} from '@angular/material/dialog';
+import {ChangePasswordComponent} from '../change-password/change-password.component';
 import {AuthService} from '../shared/services/auth.service';
 
 @Component({
@@ -8,9 +11,44 @@ import {AuthService} from '../shared/services/auth.service';
 })
 export class FacultyLayoutComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  profile: any;
+  sideMenuOpened = true;
+  sideMenuMode: 'over' | 'side' = 'over';
+
+  constructor(
+    public authService: AuthService,
+    public dialog: MatDialog,
+    public mediaObserver: MediaObserver,
+  ) {
+    this.profile = this.authService.getUserProfile();
+
+    mediaObserver.asObservable().subscribe((mediaChange) => {
+      const screen = mediaChange[0].mqAlias;
+      if (screen === 'xs') {
+       this.sideMenuMode = 'over';
+       this.sideMenuOpened = false;
+      } else {
+       this.sideMenuMode = 'side';
+       this.sideMenuOpened = true;
+      }
+     });
+  }
 
   ngOnInit(): void {
+  }
+
+  openChangePasswordDialog(): void {
+    const dialogRef = this.dialog.open(ChangePasswordComponent, {
+      width: '450px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+    });
+  }
+
+  navigate(path: string): void {
+
   }
 
 }
