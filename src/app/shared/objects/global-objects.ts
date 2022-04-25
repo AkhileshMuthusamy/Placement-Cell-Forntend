ba4bae005bba0062d7106bf20919a7467ad4e454
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-04-24 20:01:22 -0400

    Added 12th mark field

--- src/app/shared/objects/global-objects.ts
+++ src/app/shared/objects/global-objects.ts
@@ -16,6 +16,7 @@ export interface User {
     'hasNotified': boolean;
     'skills': Array<string>;
     'cgpa': number;
+    'hsMark': number;
     'disabled': boolean;
     'department': string;
     'batch': string;
